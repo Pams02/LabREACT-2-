88224297f98d11dbe5a07163c5f73371a131efe0
Pamela <pammora02@outlook.com>
2024-07-16 01:17:11 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const titulo = "Componentes de React";
+  const caracteristicas = ["Reutilizables","Reusables"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <article>
+    <h1 className="titulo">{titulo}</h1>
+    <ul className="caracteristicas">
+        {caracteristicas.map((elemento,index) => (
+          <li key={index}>{elemento}</li>
+        ))}
+    </ul>
+</article>
+
   );
 }
 
